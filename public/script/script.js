6deda5d81b75165ef7fa24b1ae2dd12ab17f993f
codingLRG <flinglion@gmail.com>
2023-08-09 17:09:16 -0700

    Adjusted a lot of the Bootstrap5 stuff, mainly colors and functions

---
+++ public/script/script.js
@@ -0,0 +1,25 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
